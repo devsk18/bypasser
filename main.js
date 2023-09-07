d594b3158b3934c8c608e0531976b57a6995409e
devsk18 <kthampansam18@gmail.com>
2023-09-07 13:58:26 +0530

    UI/UX changes

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ form.addEventListener("submit", function (event) {
 
   const videoPlayer = document.getElementById("videoPlayer");
   const videoContainer = document.getElementById("videoContainer");
+  const details = document.getElementById("details");
   const filename = document.getElementById("filename");
   const filesize = document.getElementById("filesize");
   const error = document.getElementById("error");
+  const link = document.getElementById("link");
+  const spinner = document.getElementById("spinner");
+
+  spinner.style.display = "block";
+  details.style.display = "none";
+  videoContainer.style.display = "none";
 
   const full_url = "http://wibu-api.eu.org/api/bypass/terabox?url=" + url;
 
@@ -20,15 +27,19 @@ form.addEventListener("submit", function (event) {
     .then((data) => {
       if(error.innerText != '') error.innerText = '';
       res = JSON.parse(data.contents);
-      //console.log(res.result[0].url, res.title, res.total_size);
+      console.log(res,"\n", res.result[0].url,"\n",res.title, res.total_size);
       videoPlayer.src = res.result[0].url;
-      filename.innerHTML = "File Name : " + res.title;
-      filesize.innerHTML =
-        "File Size : " + Math.round(res.total_size / (1024 * 1024)) + " Mb";
-      videoContainer.style.display = "block";
+      link.href = res.result[0].url;
+      setTimeout(()=>{
+        spinner.style.display = "none";
+        filename.innerHTML = "File Name : " + res.title;
+        filesize.innerHTML = "File Size : " + Math.round(res.total_size / (1024 * 1024)) + " Mb";
+        details.style.display = "block";
+        videoContainer.style.display = "block";
+      },1000)
     })
     .catch((err) => {
       error.innerText = "Error Occured";
-      console.log(err);
+      error.innerText = err;
     });
 });
